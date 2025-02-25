b61bcdd4b54b474f559a67f84c8440101c7249b7
thenam2kx <thenam2kx@gmail.com>
2025-02-25 23:58:15 +0700

    signout

--- src/app/(tabs)/account.tsx
+++ src/app/(tabs)/account.tsx
@@ -1,16 +1,33 @@
 import { useCurrentApp } from "@/context/app.context";
 import { getBaseUrlBackend } from "@/utils/helper";
 import { APP_COLOR } from "@/theme/theme";
-import { View, Text, Image, Pressable } from "react-native";
+import { View, Text, Image, Pressable, Alert } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import Feather from "@expo/vector-icons/Feather";
 import { router } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const AccountPage = () => {
   const { appState } = useCurrentApp();
   const baseImage = `${getBaseUrlBackend()}/images/avatar`;
   const insets = useSafeAreaInsets();
+
+  const handleLogout = async () => {
+    Alert.alert("Đăng xuất", "Bạn có chắc chắn muốn đăng xuất ?", [
+      {
+        text: "Hủy",
+        style: "cancel",
+      },
+      {
+        text: "Đồng ý",
+        onPress: async () => {
+          await AsyncStorage.removeItem("access_token");
+          router.replace("/(auth)/signin");
+        },
+      },
+    ]);
+  }
 
   return (
     <View style={{ flex: 1 }}>
@@ -147,6 +164,7 @@ const AccountPage = () => {
         }}
       >
         <Pressable
+          onPress={handleLogout}
           style={({ pressed }) => ({
             opacity: pressed === true ? 0.5 : 1,
             padding: 10,
@@ -165,7 +183,7 @@ const AccountPage = () => {
           </Text>
         </Pressable>
         <Text style={{ textAlign: "center", color: APP_COLOR.GREY }}>
-          Version 1.0 - @duvfulls
+          Version 1.0 - @devfulls
         </Text>
       </View>
     </View>
